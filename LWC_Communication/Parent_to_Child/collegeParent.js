402df45706d4e6fe4c4251ec76dffc568670cfc4
Gaurav <49844814+Kgaurav-Sfdc-Dev@users.noreply.github.com>
2022-05-04 22:18:56 +0530

    Update collegeParent.js

--- LWC_Communication/Parent_to_Child/collegeParent.js
+++ LWC_Communication/Parent_to_Child/collegeParent.js
@@ -1,20 +1,11 @@
 import { LightningElement } from 'lwc';
-
 export default class CollegeParent extends LightningElement {
-
-
-
+  
 CollegeListIndia =[{name: "IITG",location :"Guwahati" ,numberOfBranch :14 ,seats :450},
                   {name: "DSCE",location :"blr" ,numberOfBranch :19 ,seats :750},
                   {name: "Bit",location :"blr" ,numberOfBranch :20 ,seats :650},
                   {name: "Rnsit",location :"blr" ,numberOfBranch :14 ,seats :750} ,
                   {name: "IITB",location :"Mumbai" ,numberOfBranch :14 ,seats :850},
                   {name: "IITG",location :"Guwahati" ,numberOfBranch :14 ,seats :950}
-                   
-                   
-
-
-
-
    ];
 }
