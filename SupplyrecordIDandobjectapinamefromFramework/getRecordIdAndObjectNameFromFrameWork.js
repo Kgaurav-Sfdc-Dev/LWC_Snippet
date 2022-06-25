485fb13355ee9b9c6be46c789cf2a28da91a54e8
Gaurav <49844814+Kgaurav-Sfdc-Dev@users.noreply.github.com>
2022-06-25 12:48:41 +0530

    Add files via upload

---
+++ SupplyrecordIDandobjectapinamefromFramework/getRecordIdAndObjectNameFromFrameWork.js
@@ -0,0 +1,9 @@
+import { LightningElement,api } from 'lwc';
+
+export default class GetRecordIdAndObjectNameFromFrameWork extends LightningElement {
+
+    @api recordId;
+    @api objectApiName;
+
+    
+}
